083d8f25b1016455b3e8f9cf5b65cf8dacc0793c
Nguyen Dinh Dat <datndd2604@gmail.com>
2020-10-19 22:44:21 +0700

    Add project files.

---
+++ eShopSolution.Data/EF/EShopDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopDbContext : DbContext
+    {
+        public EShopDbContext( DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
