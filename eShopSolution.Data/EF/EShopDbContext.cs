ffa9743748cbdbf9cd7da2f772a93fc9495ffdbe
Nguyen Dinh Dat <datndd2604@gmail.com>
2020-10-20 00:50:30 +0700

    add seed data

--- eShopSolution.Data/EF/EShopDbContext.cs
+++ eShopSolution.Data/EF/EShopDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using eShopSolution.Data.Configurations;
 using eShopSolution.Data.Entities;
+using eShopSolution.Data.Extensions;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -32,6 +33,8 @@ namespace eShopSolution.Data.EF
             modelBuilder.ApplyConfiguration(new TransactionConfiguration());
 
             //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Seed();
         }
 
         public DbSet<Product> Products { get; set; }
