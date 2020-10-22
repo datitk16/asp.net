e6833a01955846038168cc3d4b89b0eeb7886623
Nguyen Dinh Dat <datndd2604@gmail.com>
2020-10-23 00:14:40 +0700

    create application layer

--- eShopSolution.Utilities/Exceptions/EShopException.cs
+++ eShopSolution.Utilities/Exceptions/EShopException.cs
@@ -4,8 +4,20 @@ using System.Text;
 
 namespace eShopSolution.Utilities.Exceptions
 {
-   public class EShopException
+    public class EShopException : Exception
     {
-        
+        public EShopException()
+        {
+        }
+
+        public EShopException(string message)
+            : base(message)
+        {
+        }
+
+        public EShopException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
     }
 }
